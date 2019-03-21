d625b4d3d1bca68207929c681c011cf02f571727
vocafeuvre <hirafura96@gmail.com>
2019-03-21 21:35:27 +0800

    Initialized api route

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,11 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->group('/api', function () use($app) {
+    $app->get('/[{name}]', function (Request $request, Response $response) {
+        return json_encode([
+            'greeting' => 'Hello '.$request->getAttribute('name').'!'
+        ]);
+    });
+});

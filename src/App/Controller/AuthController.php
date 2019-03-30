331c334f53c2c71350878bc11f2a22e4285e1a03
vocafeuvre <hirafura96@gmail.com>
2019-03-30 17:38:21 +0800

    Implemented register and login features in Controller and Service

--- src/App/Controller/AuthController.php
+++ src/App/Controller/AuthController.php
@@ -1,24 +1,55 @@
 <?php
 
-namespace Api\Controller;
+namespace App\Controller;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 use App\Service\AuthService;
+use App\Helper\Deserializer;
+
+use App\ModelData\UserData;
+use App\ModelData\AuthData;
 
 class AuthController {
     private $authService;
+    private $deserializer;
 
-    public function __construct(AuthService $authService){
+    public function __construct(AuthService $authService, Deserializer $deserializer){
         $this->authService = $authService;
+        $this->deserializer = $deserializer;
     }
 
     public function register(ServerRequestInterface $request, ResponseInterface $response){
+        $requestBody = $request->getParsedBody();
 
+        $modelData = $this->deserializer->deserialize(new UserData(), $requestBody);
+
+        try {
+            $responseData = $this->authService->register($modelData);
+        } catch (\Exception $e) {
+            $responseData = [
+                'error' => [
+                    'message' => $e->message,
+                    'code' => $e->code
+                ]
+            ];
+        }
+
+        return $response->withJson([
+            'data' => $responseData
+        ], 200);
     }
 
     public function login(ServerRequestInterface $request, ResponseInterface $response){
-        
+        $requestBody = $request->getParsedBody();
+
+        $modelData = $this->deserializer->deserialize(new AuthData(), $requestBody);
+
+        $responseData = $this->authService->login($modelData);
+
+        return $response->withJson([
+            'data' => $responseData
+        ], 200);
     }
 }
--- src/App/Service/AuthService.php
+++ src/App/Service/AuthService.php
@@ -2,21 +2,42 @@
 
 namespace App\Service;
 
-use Doctrine\ORM\EntityManager;
-use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Transformer\UserTransformer;
+use App\ModelData\UserData;
+use App\ModelData\AuthData;
+use App\Helper\Messages;
 
 class AuthService {
-    private $manager;
+    private $repo;
+    private $transformer;
 
-    public function __construct(EntityManager $em){
-        $this->manager = $em;
+    public function __construct(UserRepository $repo, UserTransformer $transformer){
+        $this->repo = $repo;
+        $this->transformer = $transformer;
     }
 
-    public function register(User $user){
-
+    public function register(UserData $data){
+        $savedUser = $this->repo->save($data);
+        
+        return [
+            'user' => $this->transformer->transform($savedUser)
+        ];
     }
 
-    public function login(User $user){
+    public function login(AuthData $data){
+        $user = $this->repo->fetchUserByAccount($data);
 
+        if(empty($user)) {
+            return [
+                'error' => [
+                    'message' => Messages::ACCOUNT_DOESNT_EXIST
+                ]
+            ];
+        }
+
+        return [
+            'user' => $this->transformer->transform($user)
+        ];
     }
 }

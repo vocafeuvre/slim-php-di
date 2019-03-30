3ebf4f421fecbd69fe007b5cd738543519cbb830
vocafeuvre <hirafura96@gmail.com>
2019-03-30 17:38:34 +0800

    Exposed auth endpoints in routes

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,15 @@ use Psr\Http\Message\ResponseInterface;
 
 use App\Controller\HomeController;
 use App\Controller\ApiController;
+use App\Controller\AuthController;
 
 // Routes
 $app->get('/[{name}]', [HomeController::class, 'home']);
 
 $app->group('/api', function () use($app) {
     $app->get('/{name}', [ApiController::class, 'hello']);
+    $app->group('/auth', function () use ($app) {
+        $app->post('/register', [AuthController::class, 'register']);
+        $app->post('/login', [AuthController::class, 'login']);
+    });
 });
